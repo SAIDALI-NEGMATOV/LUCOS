c9c6d767f3a5c0d2cc3c452c8e9d7f21cf774576
Saidali Negmatov <114974636+SAYYIDALI2706@users.noreply.github.com>
2023-03-04 19:20:26 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Content from './Components/Content/Content';
+import Header from './Components/header/header';
+import Mission from './Components/Mission/Mission';
+import Nav from './Components/Nav/Nav';
+import Our from './Components/Our/Our';
+import Snig from './Components/snig/Snig';
+import Test from './Components/Test/test';
+
+function App() {
+  return (
+    <div className="container">
+      <Nav/>
+      <Header/>
+      <Content/>
+      <Mission/>
+      <Our/>
+      <Test/>
+      <Snig/>
+    </div>
+  );
+}
+
+export default App;
